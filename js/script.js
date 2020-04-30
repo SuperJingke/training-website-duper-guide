51be226710da98829780577d0744e870a1bc59db
Gillian Liang <Veladaraston@Gillians-MacBook-Pro.local>
2020-04-29 23:21:56 -0400

    update

--- js/script.js
+++ js/script.js
@@ -128,3 +128,27 @@ $(function () {
   };
 
 })(window.jQuery, window, document);
+
+
+// slideshow
+var slideIndex = 0;
+showSlides();
+
+function showslides(){
+  var i;
+  var slides = document.getElementsByClassName('mySlides');
+  var dots = document.getElementsByClassName('dot');
+  for (i = 0 ;i < slides.length; i++){
+    slides[i].getElementsByClassName.display = "none";
+
+  }
+  slideIndex++;
+  if (slideIndex > slides.length){slideIndex = 1}
+  for (i = 0; i < dots.length; i++){
+    dots[i].className = dots[i].className.replace("active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += "active";
+  setTimeout(showSlides, 2000); //change image every 2 seconds
+
+}
