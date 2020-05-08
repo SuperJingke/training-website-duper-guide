3a9dca0875cc6993cfe365e09b896086d39a94ee
Gillian Liang <Veladaraston@Gillians-MacBook-Pro.local>
2020-05-08 14:20:40 -0400

    build up the web frame

--- js/script.js
+++ js/script.js
@@ -129,26 +129,28 @@ $(function () {
 
 })(window.jQuery, window, document);
 
-
-// slideshow
-var slideIndex = 0;
-showSlides();
-
-function showslides(){
-  var i;
-  var slides = document.getElementsByClassName('mySlides');
-  var dots = document.getElementsByClassName('dot');
-  for (i = 0 ;i < slides.length; i++){
-    slides[i].getElementsByClassName.display = "none";
-
+// tabs
+function openPage(pageName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length){slideIndex = 1}
-  for (i = 0; i < dots.length; i++){
-    dots[i].className = dots[i].className.replace("active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += "active";
-  setTimeout(showSlides, 2000); //change image every 2 seconds
 
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
 }
+
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+
